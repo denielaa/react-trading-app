a8ad2dfab7aa6245575035a5b2929691c63a5a28
Deniel Ariesta <deniel.ariesta@gmail.com>
2022-02-23 04:11:00 +0700

    feat: randomizer update

--- src/Projects/TradingPlan/index.tsx
+++ src/Projects/TradingPlan/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react'
+import { useState, useEffect, useCallback } from 'react'
 import { AgGridReact } from 'ag-grid-react'
 import { ExchangeType, InstrumentTypes } from 'shared/constants/exchanges'
 
@@ -37,34 +37,61 @@ const columns = [
   { field: 'ask' }
 ]
 
+const exchangeRandomizer = () => {
+  const exchanges = [ExchangeType.BINANCE, ExchangeType.FTX]
+  return exchanges[Math.floor(Math.random() * exchanges.length)]
+}
+
+const instrumentRandomizer = exchange => {
+  const instruments = InstrumentTypes[exchange]
+  return instruments[Math.floor(Math.random() * instruments.length)]
+}
+
+const getInitialData = () => {
+  // create 5 sample data
+  const data = []
+  for (let i = 0; i < 5; i++) {
+    const exchange = exchangeRandomizer()
+    data.push({
+      id: i,
+      execute: false,
+      exchange,
+      instrument: instrumentRandomizer(exchange),
+      quantity: Math.floor(Math.random() * 100),
+      bid: Math.floor(Math.random() * 100),
+      ask: Math.floor(Math.random() * 100)
+    })
+  }
+  return data
+}
+
 export const TradingPlan = () => {
-  // set to default data
-  const [rowData, setRowData] = useState([
-    {
-      execute: '',
-      exchange: 'FTX',
-      instrument: 'BTC-USDT',
-      quantity: '249.9045',
-      bid: '2.93674',
-      ask: '2.93769'
-    },
-    {
-      execute: '',
-      exchange: 'BINANCE',
-      instrument: 'BTC-USD',
-      quantity: '249.9045',
-      bid: '2.93674',
-      ask: '2.93769'
-    },
-    {
-      execute: '',
-      exchange: 'FTX',
-      instrument: 'BTC-USDT',
-      quantity: '500',
-      bid: '2.93674',
-      ask: '2.93769'
-    }
-  ])
+  const [rowData, setRowData] = useState(getInitialData())
+
+  const updatePrices = useCallback(() => {
+    const newStore = []
+    rowData.forEach(function (item) {
+      newStore.push({
+        id: item.id,
+        execute: '',
+        exchange: item.exchange,
+        instrument: item.instrument,
+        quantity: item.quantity,
+        bid: Math.floor(Math.random() * 100),
+        ask: Math.floor(Math.random() * 100)
+      })
+    })
+    setRowData(newStore)
+  }, [rowData])
+
+  const getRowNodeId = useCallback(data => {
+    return data.id
+  }, [])
+
+  useEffect(() => {
+    const interval = setInterval(updatePrices, 2000)
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <div>
@@ -85,13 +112,15 @@ export const TradingPlan = () => {
           </button>
         </div>
       </div>
+
       <div className="ag-theme-alpine" style={{ height: 500 }}>
         <AgGridReact
-          pagination={true}
           defaultColDef={{ sortable: true, filter: true }}
           rowData={rowData}
           columnDefs={columns}
           immutableData={true}
+          animateRows={true}
+          getRowNodeId={getRowNodeId}
         ></AgGridReact>
       </div>
     </div>

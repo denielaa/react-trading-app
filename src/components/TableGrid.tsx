ca21af7c223b86db4c87b64194c6cfcad5a43e23
Deniel Ariesta <deniel.ariesta@gmail.com>
2022-02-22 11:07:46 +0700

    feat: set sample row data and columns

--- src/components/TableGrid.tsx
+++ src/components/TableGrid.tsx
@@ -5,22 +5,53 @@ import 'ag-grid-community/dist/styles/ag-grid.css'
 import 'ag-grid-community/dist/styles/ag-theme-alpine.css'
 
 const InitialRowData = [
-  { make: 'Toyota', model: 'Celica', price: 35000 },
-  { make: 'Ford', model: 'Mondeo', price: 32000 },
-  { make: 'Porsche', model: 'Boxter', price: 72000 }
+  {
+    execute: '',
+    exchange: 'Kucoin Spot',
+    instrument: 'MANA-USDT',
+    quantity: '249.9045',
+    bid: '2.93674',
+    ask: '2.93769'
+  },
+  {
+    execute: '',
+    exchange: 'Kucoin Spot',
+    instrument: 'MANA-USDT',
+    quantity: '249.9045',
+    bid: '2.93674',
+    ask: '2.93769'
+  },
+  {
+    execute: '',
+    exchange: 'Kucoin Spot',
+    instrument: 'MANA-USDT',
+    quantity: '249.9045',
+    bid: '2.93674',
+    ask: '2.93769'
+  }
 ]
 
-export function TableGrid() {
+const columns = [
+  { field: 'execute' },
+  { field: 'exchange' },
+  { field: 'instrument' },
+  { field: 'quantity' },
+  { field: 'bid' },
+  { field: 'ask' }
+]
+
+export const TableGrid = () => {
   // set to default data
   const [rowData, setRowData] = useState(InitialRowData)
 
   return (
     <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
-      <AgGridReact rowData={rowData}>
-        <AgGridColumn field="make"></AgGridColumn>
-        <AgGridColumn field="model"></AgGridColumn>
-        <AgGridColumn field="price"></AgGridColumn>
-      </AgGridReact>
+      <AgGridReact
+        pagination={true}
+        defaultColDef={{ sortable: true, filter: true }}
+        rowData={rowData}
+        columnDefs={columns}
+      ></AgGridReact>
     </div>
   )
 }

bb4133936bae0aa4f4db54e63cf8b140099b1597
Deniel Ariesta <deniel.ariesta@gmail.com>
2022-02-23 08:51:44 +0700

    chore: update comment

--- src/shared/hooks/interval.ts
+++ src/shared/hooks/interval.ts
@@ -1,6 +1,6 @@
 import { useEffect, useRef } from 'react'
 
-// Copied from https://overreacted.io/making-setinterval-declarative-with-react-hooks/
+// Reference: https://overreacted.io/making-setinterval-declarative-with-react-hooks/
 const useInterval = (callback, delay) => {
   const savedCallback = useRef()
 
